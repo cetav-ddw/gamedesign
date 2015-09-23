57870882c480911b7c28f24b991c9a7659473929
tapon0509 <tapon0509@gmail.com>
2015-09-23 15:54:05 -0600

    Galeria modificada

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ window.onload = function(){
         var options =
         {
             srcNode: 'img',             // grid items (class, node)
-            margin: '20px',             // margin in pixel, default: 0px
+            margin: '5px',             // margin in pixel, default: 0px
             width: '100px',             // grid item width in pixel, default: 220px
             max_width: '500px',              // dynamic gird item width if specified, (pixel)
             resizable: true,            // re-layout if window resize
             transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
         }
-        document.querySelector('.galeria').gridify(options);
+        document.querySelector('.grid').gridify(options);
     }
