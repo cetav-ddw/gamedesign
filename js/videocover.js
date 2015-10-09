0f755a65c000d4e711c1678fc309379c40ad02d0
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-10-09 05:13:41 -0600

    nueva funcion de cover

--- js/videocover.js
+++ js/videocover.js
@@ -1,92 +1,25 @@
-$(function() {
-    var player = $('iframe');
-    var playerOrigin = '*';
-    var status = $('.status');
+function videoCover() {
+    // api vars
+    var iframe = document.getElementById( 'video' );
+    var player = $f( iframe );
 
-    var coverImg = $('.video-cover');
-    var videoLabel = $('#play-btn-label');
-    var playBtn = $('#play-btn');
-    var playerFrame = $('iframe');
+    // content to hide on click
+    var coverImg = document.getElementById( 'video-cover' );
+    var videoLabel = document.getElementById( 'play-btn-label' );
+    var playBtn = document.getElementById( 'play-btn' );
+    var playerFrame = document.getElementById( 'iframe' );
 
-    // Listen for messages from the player
-    if (window.addEventListener) {
-        window.addEventListener('message', onMessageReceived, false);
-    }
-    else {
-        window.attachEvent('onmessage', onMessageReceived, false);
-    }
-
-    // Handle messages received from the player
-    function onMessageReceived(event) {
-        // Handle messages from the vimeo player only
-        if (!(/^https?:\/\/player.vimeo.com/).test(event.origin)) {
-            return false;
-        }
-        
-        if (playerOrigin === '*') {
-            playerOrigin = event.origin;
-        }
-        
-        var data = JSON.parse(event.data);
-        
-        switch (data.event) {
-            case 'ready':
-                onReady();
-                break;
-               
-            case 'playProgress':
-                onPlayProgress(data.data);
-                break;
-                
-            case 'pause':
-                onPause();
-                break;
-               
-            case 'finish':
-                onFinish();
-                break;
-        }
-    }
+    function hideCoverContent(){
+        coverImg.setAttribute('display','none');
+        videoLabel.setAttribute('display','none');
+        playBtn.setAttribute('display','none'); 
+    };
 
     // Call the API when a button is pressed
-    $('button').on('click', function() {
-        post($(this).text().toLowerCase());
-        coverImg.hide();
-        videoLabel.hide();
-        playBtn.hide();
-    });
+    playBtn.addEventListener( 'click', function(){
+        player.api( 'play' );
+        hideCoverContent();
+    } );
+};
 
-    // Helper function for sending a message to the player
-    function post(action, value) {
-        var data = {
-          method: action
-        };
-        
-        if (value) {
-            data.value = value;
-        }
-        
-        var message = JSON.stringify(data);
-        player[0].contentWindow.postMessage(data, playerOrigin);
-    }
-
-    function onReady() {
-        status.text('ready');
-        
-        post('addEventListener', 'pause');
-        post('addEventListener', 'finish');
-        post('addEventListener', 'playProgress');
-    }
-
-    function onPause() {
-        status.text('paused');
-    }
-
-    function onFinish() {
-        status.text('finished');
-    }
-
-    function onPlayProgress(data) {
-        status.text(data.seconds + 's played');
-    }
-});
+videoCover();
